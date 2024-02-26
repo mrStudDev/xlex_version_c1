0c2bc27e17288a8e75b7404c573a226df9f3ee53
mrStudDev <marcosscfloripa@gmail.com>
2024-02-26 02:04:42 -0300

    lolo

--- xlexapp/settings.py
+++ xlexapp/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'dados_prod_xlex_c1',
         'USER': 'postgres',
         'PASSWORD': 'rmmr7117urpdfg56r',
-        'HOST': '0.0.0.0/0',  
+        'HOST': 'localhost',  
         'PORT': '5432',
     }
 }
